967ac37241950e4b723813a27ecc192e8a7edf7a
Aurelio-Teliti_ElGhEmu <aurelio.teliti@luxottica.com>
2025-08-18 20:19:10 +0200

    Fork from nvchad

---
+++ lua/plugins/init.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    event = "BufWritePre",
+    opts = require "configs.conform",
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  -- test new blink
+  -- { import = "nvchad.blink.lazyspec" },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "python",
+      },
+    },
+  },
+}
