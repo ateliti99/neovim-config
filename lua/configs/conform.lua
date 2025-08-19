d08bc2ac0a55a78e0eb2230cf5959e593c80885c
Aurelio-Teliti_ElGhEmu <aurelio.teliti@luxottica.com>
2025-08-19 18:54:33 +0200

    Add formatter for Python and sync Lazy

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,13 +1,33 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
+    python = { "isort", "black" },
     -- css = { "prettier" },
     -- html = { "prettier" },
   },
 
+  -- Configure individual formatters
+  formatters = {
+    black = {
+      command = "black",
+      args = function(ctx)
+        local fname = ctx and ctx.filename or vim.api.nvim_buf_get_name(ctx.bufnr or 0)
+        return { "--quiet", "--line-length", "88", "--stdin-filename", fname, "-" }
+      end,
+    },
+
+    isort = {
+      command = "isort",
+      args = function(ctx)
+        local fname = ctx and ctx.filename or vim.api.nvim_buf_get_name(ctx.bufnr or 0)
+        return { "--profile", "black", "--line-width", "88", "-" }
+      end,
+    },
+  },
+
   format_on_save = {
     -- These options will be passed to conform.format()
-    timeout_ms = 500,
+    timeout_ms = 1000,
     lsp_fallback = true,
   },
 }
---
+++ lua/configs/test.py
@@ -0,0 +1,6 @@
+import abc
+import tabnanny
+from pathlib import Path
+
+for i in range(10):
+    print("hello!")
